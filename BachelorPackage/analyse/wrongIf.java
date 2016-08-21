9a167820af2c55cda019469dd4280dacffcb1b36
Simon Müller <simonmueller1337@gmail.com>
2016-08-21 20:52:00 +0200

    Finished Problem description and analysis. Most probably removing point 2.4.

---
+++ BachelorPackage/analyse/wrongIf.java
@@ -0,0 +1,10 @@
+public class Example {
+	public static void main(String[] args) {
+		int r = 5;
+		r *= 3 + 7;
+		if (r == 22)
+			System.out.println("Erfolg");
+		else
+			System.out.println("Fehler");
+	}
+}
---
+++ BachelorPackage/analyse/wrongLoop.java
@@ -0,0 +1,8 @@
+public class Example {
+	public static void main(String[] args) {
+		int r = 7;
+		for (int i = 0; i > 3; i++)
+			r++;
+		System.out.println(r);
+	}
+}
